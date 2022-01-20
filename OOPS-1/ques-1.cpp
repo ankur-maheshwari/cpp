769acad7b2b3a6213a37be4c525d4c00fc166b73
Ankur Maheshwari <65300192+ankur-maheshwari@users.noreply.github.com>
2022-01-20 18:17:44 +0530

    Create ques-1.cpp

---
+++ OOPS-1/ques-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class Student {                   output = error   becz   roll no is private
+    int rollNumber;
+};
+
+int main() {
+    Student s;
+    cout << s.rollNumber;
+}
