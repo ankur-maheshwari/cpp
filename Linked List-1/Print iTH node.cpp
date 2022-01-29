a4b6eb7f99c72b74e67963c02e81b4153a34ce62
Ankur Maheshwari <65300192+ankur-maheshwari@users.noreply.github.com>
2022-01-29 17:22:48 +0530

    Create Print iTH node.cpp

---
+++ Linked List-1/Print iTH node.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+// concept
+void printIthNode(Node *head, int i)
+{
+    Node* temp = head;
+    int count =0;
+    while(temp != NULL)
+    {
+        if(i==count)
+        {
+            cout<< temp->data<<endl;
+        }  
+        count++;
+        temp=temp->next;
+    }
+}
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		int pos;
+		cin >> pos;
+		printIthNode(head, pos);
+		cout << endl;
+	}
+	return 0;
+}
