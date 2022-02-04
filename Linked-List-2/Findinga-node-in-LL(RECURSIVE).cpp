816ab79124019d56a76957635aaf8addf8c849ef
Ankur Maheshwari <65300192+ankur-maheshwari@users.noreply.github.com>
2022-02-04 13:16:24 +0530

    Create Findinga-node-in-LL(RECURSIVE).cpp

---
+++ Linked-List-2/Findinga-node-in-LL(RECURSIVE).cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+
+
+int findNodeRec(Node *head, int n){
+	if(head == NULL){
+        return -1;
+    }
+    
+	int ans = findNodeRec(head->next, n);
+    if(ans == -1){
+        if(head -> data == n){
+            return 0;
+        }
+        else return -1;
+    }
+    else{
+        return ans + 1;
+    }
+}
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		int val;
+		cin >> val;
+		cout << findNodeRec(head, val) << endl;
+	}
+}
